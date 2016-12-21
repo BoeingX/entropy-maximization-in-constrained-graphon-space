9605c4c4469cc7fb25538766152e001355dab50c
BoeingX <song.baoyang@gmail.com>
2016-12-21 14:48:57 +0100

    added initialize

---
+++ initialize.m
@@ -0,0 +1,19 @@
+function [g, c] = initialize(N, constraints, uniform_partition)
+if nargin < 3
+    uniform_partition = false;
+end
+while true
+    if uniform_partition
+        c = ones(N, 1) / N;
+    else
+        c = rand(N, 1);
+    end
+    g = rand(N);
+    g = (g + g')/2;
+    g = reshape(g, [], 1);
+    [g, c] = rescale(g, c, N, constraints.rho0, constraints.tau0);
+    if sum(g>1) == 0 && sum(g<0) == 0
+        return
+    end
+end
+end
