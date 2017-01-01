1ac87a092863ec4a9ec523e5f80e6effd06cdbec
BoeingX <song.baoyang@gmail.com>
2017-01-01 16:05:55 +0100

    update

---
+++ test/test_triangle_densities.m
@@ -0,0 +1,12 @@
+td = csvread('data/TriangleDensities.csv', 1);
+td(:, end+1) = 0.5*(td(:, 2) + td(:, 3));
+rho0 = td(10, 1);
+tau0 = td(10, end);
+constraints.rho0 = rho0;
+constraints.tau0 = tau0;
+f_opts = [];
+for N = 3:16
+    N
+    [g, c, f] = optimize(N, constraints, 10);
+    f_opts = [f_opts, f];
+end
