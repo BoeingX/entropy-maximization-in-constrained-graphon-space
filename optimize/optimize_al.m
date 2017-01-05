6749eeb7cd866b49c88e011d2f90035896bfdd5c
dex <dexiongc@gmail.com>
2017-01-05 20:35:48 +0100

    AL parameters modified

--- optimize/optimize_al.m
+++ optimize/optimize_al.m
@@ -10,14 +10,14 @@ ub = ones(nvars, 1);
 if strcmp(algorithm, 'al')
     x = rand(nvars, 1);
     y = rand(n_constraints, 1);
-    lambda_k = rand();
-    eta = 1e-6;
+    lambda_k = 10;
+    eta = 1e-8;
     eta_0 = 1e-1;
     eta_k = 1e-1;
     converged = false;
     alpha = 0.5;
     beta = 0.5;
-    t = 2;
+    t = 100;
     % equality constraints
     [Aeq_g, beq_g] = linear_constraints_eq(N, 'g');
     [Aeq_c, beq_c] = linear_constraints_eq(N, 'c'); 
@@ -31,14 +31,14 @@ if strcmp(algorithm, 'al')
 %         grad = @(u) entropy_grad(u(1:N*N), u((N*N+1):end), N) ...
 %             - y' * con_grad(u, N, Aeq) + lambda_k*con(u, N, constraints, Aeq, beq)'*con_grad(u, N, Aeq);
         func_grad = @(u) compute_func_grad(u, y, lambda_k, N, constraints, Aeq, beq);
-        options = optimoptions('fmincon','Algorithm', 'interior-point', 'Display', 'off', 'TolCon', 1e-6, 'GradObj','on');
+        options = optimoptions('fmincon','Algorithm', 'interior-point', 'Display', 'off', 'TolFun', 1e-10, 'GradObj','on');
         [x_opt, L, flag, ~, ~, z_opt, ~] = fmincon(func_grad, x, [], [], [], [], lb, ub, [], options);
         if norm(con(x_opt, N ,constraints, Aeq, beq)) < max(eta, eta_k)
             y_opt = y - lambda_k*con(x, N, constraints, Aeq, beq);
             x = x_opt;
             y = y_opt;
             z = z_opt;
-            if norm(abs(con(x, N, constraints, Aeq, beq))) < 1e-6 && (isempty(find(x < 0))) ...
+            if norm(abs(con(x, N, constraints, Aeq, beq))) < eta && (isempty(find(x < 0))) ...
                 & (isempty(find(x > 1)))
                 converged = true;
             end
@@ -48,6 +48,7 @@ if strcmp(algorithm, 'al')
             eta_k = eta_0 / (1 + lambda_k^alpha);
         end
     end
+    lambda_k
     [g, c] = get_gc(x, N);
     f = entropy(g, c, N);
     flag = 1;
